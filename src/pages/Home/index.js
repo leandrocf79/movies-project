f4352d44a0454fffe8bbce0f964fff5da43dd055
leandrocf79 <leandrocf79@gmail.com>
2023-02-07 12:13:50 -0200

    UPDATE

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -53,7 +53,7 @@ if(loading){
             return(
               <article key={filme.id}>
                  <strong>{ filme.title }</strong>
-                 <img src={`https:image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                 <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
                  <Link to ={`filme/${ filme.id }`}>Acessar</Link>
               </article>
             )
